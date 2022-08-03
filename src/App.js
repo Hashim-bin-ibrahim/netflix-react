95050cb233e675e30e074d53f8181f4f346339fb
hashim <hashim@pop-os.localdomain>
2022-08-03 10:07:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Banner from './components/Banner/Banner';
+import Navbar from './components/Navbar/Navbar';
+import './App.css'
+import {orginals,action,horror,comedy,romance} from './urls'
+import RowPoster from './components/RowPoster/RowPoster';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Banner />
+      <RowPoster url = {orginals} title = 'Netflix Orginals' />
+      <RowPoster url = {action}  title = 'Action' isSmall/>
+      <RowPoster url = {horror}  title = 'Horror' isSmall/>
+      <RowPoster url = {comedy}  title = 'Comedy' isSmall/>
+      <RowPoster url = {romance}  title = 'Romance' isSmall/>
     </div>
   );
 }
